feb7fe6ddf74dcca60e17ec22d72da5b1123cede
guany <157423346@qq.com>
2019-09-19 23:19:05 +0800

    inicommit

---
+++ Assets/Scripts/Component/AgentComponent.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Mathematics;
+
+[SerializeField]
+public struct AgentComponent : IComponentData
+{
+    public float3 position;
+    public int moveSpeed;
+}
---
+++ Assets/Scripts/System/AIAgentSystem.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Burst;
+using Unity.Mathematics;
+using Unity.Entities;
+using UnityEngine.Experimental.AI;
+using Unity.Jobs;
+using Unity.Collections;
+using Unity.Transforms;
+
+[]
+public class AIAgentSystem : JobComponentSystem
+{
+    private EntityQuery agentQuery;
+    struct CreateNavNextPointJob : IJobForEachWithEntity<AgentComponent>
+    {
+        public void Execute(Entity entity, int index, ref AgentComponent c0)
+        {
+            
+        }
+    }
+
+    protected override void OnCreate()
+    {
+        base.OnCreate();
+        agentQuery  = GetEntityQuery(new EntityQueryDesc
+        {
+            All = new[] { ComponentType.ReadWrite<AgentComponent>(), ComponentType.ReadWrite<LocalToWorld>() },
+        });
+    }
+
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        
+    }
+}
